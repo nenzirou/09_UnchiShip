19fb000dd5ecd4f3f1a03d1217aec84f2e610280
nenzirou <xbfgf419@yahoo.co.jp>
2021-01-01 13:09:38 +0900

    おじさん実装

--- typescript/ojisan.ts
+++ typescript/ojisan.ts
@@ -1,8 +1,12 @@
 import * as PIXI from "pixi.js";
+import { Ship } from "./ship";
 /*
 おじさんに持たせる機能
 id:おじさんの識別子
-
+hp:体力
+satiety:満腹度
+destiny:便意
+fatigue:疲労度
 */
 export class Ojisan extends PIXI.Sprite {
     background: PIXI.Graphics;
@@ -10,11 +14,33 @@ export class Ojisan extends PIXI.Sprite {
     hp: number = 100;
     satiety: number = 100;
     destiny: number = 0;
+    fatigue: number = 0;
     level: number = 0;
     state: string = "STOP";
+    cnt: number = 0;
+    dx: number = 0;
+    dy: number = 0;
+    nextCnt: number = 0;
     constructor(x: number, y: number) {
         super();
+        this.anchor.set(0.5);
         this.x = x;// おじさんのｘ座標
         this.y = y;// おじさんのｙ座標
+        this.texture = PIXI.Loader.shared.resources.oji.texture;
+
+    }
+    move(ship: Ship, bg: PIXI.Graphics) {
+        if (this.x < this.width / 2) this.x = this.width / 2;
+        if (this.y < this.height / 2) this.y = this.height / 2;
+        if (this.x > bg.width - this.width / 2) this.x = bg.width - this.width / 2;
+        if (this.y > bg.height - this.height / 2) this.y = bg.height - this.height / 2;
+        this.x += this.dx;
+        this.y += this.dy;
+        if (this.cnt % 300 == this.nextCnt) {
+            this.dx = Math.random() * 2 - 1;
+            this.dy = Math.random() * 2 - 1;
+            this.nextCnt = Math.floor(Math.random() * 150 + 150);
+        }
+        this.cnt++;
     }
 }
--- typescript/ship.ts
+++ typescript/ship.ts
@@ -2,6 +2,7 @@ import * as PIXI from "pixi.js";
 import { Room } from "./room";
 import { Room_wall } from "./room_wall";
 import { Room_aisle } from "./room_aisle";
+import { Ojisan } from "./ojisan";
 /*
 shipに持たせる機能
 船の全体像
@@ -9,29 +10,42 @@ componentsに各部屋のクラスを保存する
 */
 export class Ship extends PIXI.Container {
     background: PIXI.Graphics;
-    rooms: Room[] = new Array(50);
+    rooms: Room[][] = new Array();
+    ojis: Ojisan[] = new Array();
     cnt: number = 0;
     constructor(x: number, y: number, width: number, height: number) {
         super();
         this.x = x;// 船全体のｘ座標
         this.y = y;// 船全体のｙ座標
+        this.width = width;
+        this.height = height;
         this.background = new PIXI.Graphics(); // 船の背景となるオブジェクト
         this.background.beginFill(0xdddddd); // 色指定
         this.background.drawRect(0, 0, width, height); // 位置(0,0)を左上にして、width,heghtの四角形を描画
         this.background.endFill(); // 描画完了
         this.addChild(this.background); // 背景を船に適用
         for (let i = 0; i < 12; i++) {
+            this.rooms[i] = new Array();
             for (let j = 0; j < 8; j++) {
                 let room = new Room_aisle(50 * j, 50 * i);
-                this.addChild(room);
-                this.rooms.push(room);
+                this.background.addChild(room);
+                this.rooms[i].push(room);
             }
         }
-        this.scale.set(0.8,0.8);
+        for (let i = 0; i < 200; i++) {
+            let oji = new Ojisan(Math.random() * width, Math.random() * height);
+            this.addChild(oji);
+            this.ojis.push(oji);
+        }
+        this.scale.set(0.8, 0.8);
     }
     move() {
         this.cnt++;
-        this.x = Math.sin(this.cnt/300)*20+20;
-        this.y = Math.cos(this.cnt/300)*20+20;
+        //this.scale.set(0.5 + this.cnt % 100 / 200, 0.5 + this.cnt % 100 / 200);
+        this.x = Math.sin(this.cnt / 300) * 20 + 20;
+        this.y = Math.cos(this.cnt / 300) * 20 + 20;
+        for (let i = 0; i < this.ojis.length; i++) {
+            this.ojis[i].move(this,this.background);
+        }
     }
 }
